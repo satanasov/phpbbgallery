1b602df4c686a92f74a041a08cecf1cb6b902fdf
StanislavAtanasov <lucifer@anavaro.com>
2014-11-26 02:12:29 +0200

    Correct the auth issues on delete

--- core/controller/image.php
+++ core/controller/image.php
@@ -845,9 +845,9 @@ class image
 		$image_backlink = append_sid('./gallery/image/'. $image_id);
 		$album_backlink = append_sid('./gallery/album/'. $image_data['image_album_id']);
 		$this->gallery_auth->load_user_premissions($this->user->data['user_id']);
-		if (!$this->gallery_auth->acl_check('i_delete', $album_id, $image_data['image_user_id']) || ($image_status == \phpbbgallery\core\image\image::STATUS_ORPHAN))
+		if (!$this->gallery_auth->acl_check('i_delete', $album_id, $album_data['album_user_id']) || ($image_status == \phpbbgallery\core\image\image::STATUS_ORPHAN))
 		{
-			if (!$this->gallery_auth->acl_check('m_delete', $album_id, $image_data['image_user_id']))
+			if (!$this->gallery_auth->acl_check('m_delete', $album_id, $album_data['album_user_id']))
 			{
 				$this->misc->not_authorised($album_backlink, $album_loginlink, 'LOGIN_EXPLAIN_UPLOAD');
 			}
