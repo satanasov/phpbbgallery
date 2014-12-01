94ea08fe5faa69ffbbe3fe621af9ae654eddaae5
Stanislav Atanasov <lucifer@anavaro.com>
2014-12-01 13:58:01 +0200

    tests

--- travis/prepare-phpbb.sh
+++ travis/prepare-phpbb.sh
@@ -16,9 +16,9 @@ BRANCH=$2
 EXTPATH_TEMP=$3
 
 # Copy extension to a temp folder
-mkdir ../../tmp
-cp -R . ../../tmp
-cd ../../
+mkdir ../tmp
+cp -R . ../tmp
+cd ../
 
 # Clone phpBB
 git clone --depth=1 "git://github.com/phpbb/phpbb.git" "phpBB3" --branch=$BRANCH
