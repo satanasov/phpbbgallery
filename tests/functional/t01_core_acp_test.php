d2eff6c303ef61f7e9c5bc1e695f8dc20117998d
StanislavAtanasov <lucifer@anavaro.com>
2014-12-01 14:11:24 +0200

    tests

--- tests/functional/t01_core_acp_test.php
+++ tests/functional/t01_core_acp_test.php
@@ -11,7 +11,7 @@ namespace phpbbgallery\tests\functional;
 /**
 * @group functional
 */
-class acp_test extends phpbbgallery_base
+class t01_core_acp_test extends phpbbgallery_base
 {
 	public function test_acp_pages()
 	{
