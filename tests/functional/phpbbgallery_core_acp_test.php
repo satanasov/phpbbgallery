d4cac93c1055b3d0095376df6b0bf639a62c4924
StanislavAtanasov <lucifer@anavaro.com>
2014-12-11 06:16:58 +0200

    Change hard coded path with route
    Add test for ACP Create album

--- tests/functional/phpbbgallery_core_acp_test.php
+++ tests/functional/phpbbgallery_core_acp_test.php
@@ -57,4 +57,30 @@ class phpbbgallery_core_acp_test extends phpbbgallery_base
 		$this->logout();
 		$this->logout();
 	}
+	
+	public function test_admin_create_album()
+	{
+		$this->login();
+		$this->admin_login();
+		
+		$this->add_lang_ext('phpbbgallery/core', 'gallery_acp');
+		$crawler = self::request('GET', 'adm/index.php?i=-phpbbgallery-core-acp-albums_module&mode=manage&sid=' . $this->sid);
+		
+		// Step 1
+		$form = $crawler->selectButton($this->lang('CREATE_ALBUM'))->form();
+		$form['album_name'] = 'First test album!';
+		$crawler = self::submit($form);
+		
+		// Step 2 - we should have reached a form for creating album_name
+		$this->assertContainsLang('ALBUM_EDIT_EXPLAIN', $crawler->text());
+		
+		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
+		$crawler = self::submit($form);
+		
+		// Step 3 - Album should be created and we should have option to add permissions
+		$this->assertContainsLang('ALBUM_CREATED', $crawler->text());
+		
+		$crawler = self::request('GET', 'adm/index.php?i=-phpbbgallery-core-acp-albums_module&mode=manage&sid=' . $this->sid);
+		$this->assertContains('First test album!', $crawler->text());
+	}
 }
