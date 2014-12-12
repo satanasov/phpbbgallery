5ba4176cb4dc442fb4362adfd9a2aa5112d111ae
Stanislav Atanasov <lucifer@anavaro.com>
2014-12-12 06:00:52 +0200

    Test

--- tests/functional/phpbbgallery_acp_test.php
+++ tests/functional/phpbbgallery_acp_test.php
@@ -106,7 +106,9 @@ class phpbbgallery_acp_test extends phpbbgallery_base
 		
 		$this->assertContains('First test album!', $crawler->text());
 		
-		$form = $crawler->filter('form[id=add_groups]')->form();
+		$select = $crawler->filter('ADD_GROUPS')->parents();
+		$select = $select->parents();
+		$form = $crawler->selectButton($this->lang('ADD_PERMISSIONS'))->form();
 		$form['group_id'] = array(5);
 		$crawler = self::submit($form);
 		
