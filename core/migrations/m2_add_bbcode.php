ade460ce1496c4db5dd01c31cbceae9ede0e803e
Stanislav Atanasov <lucifer@anavaro.com>
2014-12-12 03:30:54 +0200

    Sniffer

--- core/migrations/m2_add_bbcode.php
+++ core/migrations/m2_add_bbcode.php
@@ -29,88 +29,14 @@ class m2_add_bbcode extends \phpbb\db\migration\migration
 			array('custom', array(array(&$this, 'remove_bbcode'))),
 		);
 	}
-/*
-	public function install_bbcode()
-	{
-		// Load the acp_bbcode class
-		if (!class_exists('acp_bbcodes'))
-		{
-			include($this->phpbb_root_path . 'includes/acp/acp_bbcodes.' . $this->php_ext);
-		}
-		$bbcode_tool = new \acp_bbcodes();
 
-		$bbcode_data = array(
-			'album' => array(
-				'bbcode_helpline'	=> 'GALLERY_HELPLINE_ALBUM',
-				'bbcode_match'		=> '[album]{NUMBER}[/album]',
-				'bbcode_tpl'		=> '<a href="gallery/image/{NUMBER}"><img src="gallery/image/{NUMBER}/mini" alt="{NUMBER}" /></a>',
-			),
-		);
-		foreach ($bbcode_data as $bbcode_name => $bbcode_array)
-		{
-			// Build the BBCodes
-			$data = $bbcode_tool->build_regexp($bbcode_array['bbcode_match'], $bbcode_array['bbcode_tpl']);
-			$bbcode_array += array(
-				'bbcode_tag'			=> $data['bbcode_tag'],
-				'first_pass_match'		=> $data['first_pass_match'],
-				'first_pass_replace'	=> $data['first_pass_replace'],
-				'second_pass_match'		=> $data['second_pass_match'],
-				'second_pass_replace'	=> $data['second_pass_replace']
-			);
-			$sql = 'SELECT bbcode_id
-				FROM ' . $this->table_prefix . "bbcodes
-				WHERE LOWER(bbcode_tag) = '" . strtolower($bbcode_name) . "'
-				OR LOWER(bbcode_tag) = '" . strtolower($bbcode_array['bbcode_tag']) . "'";
-			$result = $this->db->sql_query($sql);
-			$row_exists = $this->db->sql_fetchrow($result);
-			$this->db->sql_freeresult($result);
-			if ($row_exists)
-			{
-				// Update exisiting BBCode
-				$bbcode_id = $row_exists['bbcode_id'];
-				$sql = 'UPDATE ' . $this->table_prefix . 'bbcodes
-					SET ' . $this->db->sql_build_array('UPDATE', $bbcode_array) . '
-					WHERE bbcode_id = ' . $bbcode_id;
-				$this->db->sql_query($sql);
-			}
-			else
-			{
-				// Create new BBCode
-				$sql = 'SELECT MAX(bbcode_id) AS max_bbcode_id
-					FROM ' . $this->table_prefix . 'bbcodes';
-				$result = $this->db->sql_query($sql);
-				$row = $this->db->sql_fetchrow($result);
-				$this->db->sql_freeresult($result);
-				if ($row)
-				{
-					$bbcode_id = $row['max_bbcode_id'] + 1;
-					// Make sure it is greater than the core BBCode ids...
-					if ($bbcode_id <= NUM_CORE_BBCODES)
-					{
-						$bbcode_id = NUM_CORE_BBCODES + 1;
-					}
-				}
-				else
-				{
-					$bbcode_id = NUM_CORE_BBCODES + 1;
-				}
-				if ($bbcode_id <= BBCODE_LIMIT)
-				{
-					$bbcode_array['bbcode_id'] = (int) $bbcode_id;
-					$bbcode_array['display_on_posting'] = 1;
-					$this->db->sql_query('INSERT INTO ' . $this->table_prefix . 'bbcodes ' . $this->db->sql_build_array('INSERT', $bbcode_array));
-				}
-			}
-		}
-	}
-*/
 	public function install_bbcode()
 	{
 		$sql = 'SELECT bbcode_id FROM ' . $this->table_prefix . 'bbcodes WHERE LOWER(bbcode_tag) = \'album\'';
 		$result = $this->db->sql_query($sql);
 		$row = $this->db->sql_fetchrow($result);
 		$this->db->sql_freeresult($result);
-		
+
 		if (!$row)
 		{
 			// Create new BBCode
@@ -118,7 +44,7 @@ class m2_add_bbcode extends \phpbb\db\migration\migration
 			$result = $this->db->sql_query($sql);
 			$row = $this->db->sql_fetchrow($result);
 			$this->db->sql_freeresult($result);
-			
+
 			if ($row)
 			{
 				$bbcode_id = $row['max_bbcode_id'] + 1;
@@ -132,7 +58,7 @@ class m2_add_bbcode extends \phpbb\db\migration\migration
 			{
 				$bbcode_id = NUM_CORE_BBCODES + 1;
 			}
-			
+
 			if ($bbcode_id <= BBCODE_LIMIT)
 			{
 				$this->db->sql_query('INSERT INTO ' . $this->table_prefix . 'bbcodes ' . $this->db->sql_build_array(
@@ -141,7 +67,7 @@ class m2_add_bbcode extends \phpbb\db\migration\migration
 						'bbcode_tag'			=> 'album',
 						'bbcode_id'				=> (int) $bbcode_id,
 						'bbcode_helpline'		=> 'GALLERY_HELPLINE_ALBUM',
-						'display_on_posting'	=> 0,
+						'display_on_posting'	=> 1,
 						'bbcode_match'			=> '[album]{NUMBER}[/album]',
 						'bbcode_tpl'			=> '<a href="gallery/image/{NUMBER}"><img src="gallery/image/{NUMBER}/mini" alt="{NUMBER}" /></a>',
 						'first_pass_match'		=> '!\[album\]([0-9]+)\[/album\]!i',
