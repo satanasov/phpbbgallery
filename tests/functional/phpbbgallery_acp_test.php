377eaeb1a19076b7238a9e0e2a192f6acc334726
Stanislav Atanasov <lucifer@anavaro.com>
2014-12-15 21:47:44 +0200

    :)

--- tests/functional/phpbbgallery_acp_test.php
+++ tests/functional/phpbbgallery_acp_test.php
@@ -231,9 +231,9 @@ class phpbbgallery_acp_test extends phpbbgallery_base
 		$crawler = self::request('GET', 'app.php/gallery/album/1');
 		
 		//$link = $crawler->filter('div.upload-icon > a')->attr('href');
-		$this->assertContains('lalalalalal',  $crawler->filter('div.upload-icon > a')->attr('href'));
+		/*$this->assertContains('lalalalalal',  $crawler->filter('div.upload-icon > a')->attr('href'));*/
 		
-		/*$crawler = self::request('GET', $link);
+		$crawler = self::request('GET', $crawler->filter('div.upload-icon > a')->attr('href'));
 		
 		$this->assertContainsLang('UPLOAD_IMAGE', $crawler->text());
 		
@@ -252,6 +252,6 @@ class phpbbgallery_acp_test extends phpbbgallery_base
 		
 		$this->assertContains('valid',  $crawler->filter('div.polaroid')->filter('p')->text());
 		
-		$this->logout();*/
+		$this->logout();
 	}
 }
