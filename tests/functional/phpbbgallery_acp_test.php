66ad0a055014daf9e8abdf65f04f98db14cedc06
StanislavAtanasov <lucifer@anavaro.com>
2014-12-16 10:34:43 +0200

    dis test

--- tests/functional/phpbbgallery_acp_test.php
+++ tests/functional/phpbbgallery_acp_test.php
@@ -344,7 +344,7 @@ class phpbbgallery_acp_test extends phpbbgallery_base
 		
 		$image = $crawler->filter('a:contains("Valid but needs delete")')->parents()->parents();
 		
-		$form = $image->selectButton($this->lang['DISAPPROVE'])->form();
+		$form = $image->selectButton($this->lang['APPROVE'])->form();
 		unset($form['action']);
 		$form['action'] = array('disapprove');
 		$crawler = self::submit($form);
