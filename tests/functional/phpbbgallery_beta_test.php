2822d07863bdfd340668609dc5274064db678c50
StanislavAtanasov <lucifer@anavaro.com>
2014-12-24 10:00:32 +0200

    Tests fail

--- tests/functional/phpbbgallery_beta_test.php
+++ tests/functional/phpbbgallery_beta_test.php
@@ -50,8 +50,8 @@ class phpbbgallery_beta_test extends phpbbgallery_base
 		
 		$this->assertContainsLang('EXTENSION_DISABLE_SUCCESS', $crawler->text());
 		
-		$crawler = self::request('GET', 'index.php&sid=' . $this->sid);
-		$this->assertContainsLang('BOARD_INDEX', $crawler->text());
+		$crawler = self::request('GET', 'adm/index.php&sid=' . $this->sid);
+		$this->assertContainsLang('Welcome to phpBB', $crawler->text());
 		
 		// If all works - enable all and continue testing
 		
