b62483cd57aff6f82c02c89d21d9f752ca900200
Stanislav Atanasov <lucifer@anavaro.com>
2014-12-24 09:51:49 +0200

    Add core function to disable all add-ons Add Test for disable

--- core/ext.php
+++ core/ext.php
@@ -8,4 +8,32 @@ namespace phpbbgallery\core;
 
 class ext extends \phpbb\extension\base
 {
+	protected $add_ons = array(
+		'phpbbgallery/acpimport',
+		'phpbbgallery/exif',
+	);
+	/**
+	* Single disable step that does nothing
+	*
+	* @param mixed $old_state State returned by previous call of this method
+	* @return mixed Returns false after last step, otherwise temporary state
+	*/
+	function disable_step($old_state)
+	{
+		switch ($old_state)
+		{
+			case '': // Empty means nothing has run yet
+				// Disable list of official extensions
+				$extensions = $this->container->get('ext.manager');
+				foreach ($this->add_ons as $var)
+				{
+					$extensions->disable($var);
+				}
+			default:
+				// Run parent disable step method
+				return parent::disable_step($old_state);
+			break;
+		}
+	}
+
 }
