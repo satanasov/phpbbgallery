641884ec48bfd3545377c08e2c968b213570c1a6
StanislavAtanasov <lucifer@anavaro.com>
2014-12-25 11:38:46 +0200

    Revert "a var dump ..."
    This reverts commit cbc9a56017ef241bcbda0cb236eff042133b08b8.

--- tests/functional/phpbbgallery_beta_test.php
+++ tests/functional/phpbbgallery_beta_test.php
@@ -32,36 +32,5 @@ class phpbbgallery_beta_test extends phpbbgallery_base
 		$object = $crawler->filter('a:contains("Valid")')->parents()->parents();
 		$link = $object->filter('img')->parents()->attr('href');
 		$this->assertContains('/source', $link);
-		$this->logout();
-	}
-	
-	// Test if disabling core will disable all other extensions
-	public function test_disable_core()
-	{
-		$this->login();
-		$this->admin_login();
-		$this->add_lang_ext('phpbbgallery/core', 'gallery');
-		$this->add_lang('common');
-		$this->add_lang('acp/extensions');
-		
-		$crawler = self::request('GET', 'adm/index.php?i=acp_extensions&mode=main&action=disable_pre&ext_name=phpbbgallery%2Fcore&sid=' . $this->sid);
-		$form = $crawler->selectButton($this->lang('DISABLE'))->form();
-		$crawler = self::submit($form);
-		
-		$this->assertContainsLang('EXTENSION_DISABLE_SUCCESS', $crawler->text());
-		
-<<<<<<< Updated upstream
-		//$crawler = self::request('GET', 'adm/index.php&i=21&sid=' . $this->sid);
-		//$this->assertContainsLang('EXTENSIONS_ADMIN', $crawler->text());
-=======
-		$crawler = self::request('GET', 'index.php&sid=' . $this->sid);
-		$this->assertContainsLang('BOARD_INDEX', $crawler->text());
->>>>>>> Stashed changes
-		
-		// If all works - enable all and continue testing
-		
-		$this->setUp();
-		
-		$this->logout();
 	}
 }
