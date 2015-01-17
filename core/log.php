09f9c7eba0fddd50eb39105fee9b80d567efe43e
StanislavAtanasov <lucifer@anavaro.com>
2015-01-17 04:07:38 +0200

    Add Basic log class Add add_log function

---
+++ core/log.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+*
+* @package phpBB Gallery
+* @copyright (c) 2014 Lucifer
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace phpbbgallery\core;
+
+class log
+{
+	public function __contruct(\phpbb\db\driver\driver_interface $db, \phpbb\user $user,
+	$log_table)
+	{
+		$this->db = $db;
+		$this->user = $user;
+		$this->log_table = $log_table;
+	}
+
+	/**
+	* Add item to log
+	*
+	* @param	(string)	$log_type		type of action (user/mod/admin/system) max 16 chars
+	* @param	(string)	$log_action		action we are loging (add/remove/approve/unaprove/delete) max 32 chars
+	* @param	(int)		$album			Album we are loging for (can be 0)
+	* @param	(int)		$image			Image we are loging for (can be 0)
+	* @param	(string)	$description	Description sting
+	*/
+	
+	public function add_log($log_type, $log_action, $album = 0, $image = 0, $description = '')
+	{
+		$user = (int) $this->user->data['user_id'];
+		$time = (int) time();
+		
+		$sql = 'INSERT INTO ' . $this->log_table . ' (log_time, log_type, log_action, user, album, image, description) 
+		VALUES (
+			' . (int) $time . ',
+			\'' . $this->db->sql_escape($log_type) . '\',
+			\'' . $this->db->sql_escape($log_action) . '\',
+			' . (int) $album . ',
+			' . (int) $image . ',
+			\'' . $this->db->sql_escape($description) . '\'
+		)';
+		$this->db->sql_query($sql);
+	}
+}
