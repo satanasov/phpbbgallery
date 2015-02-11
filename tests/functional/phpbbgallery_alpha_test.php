13a23a119efcc8928e50fa4c88eea690d1f194e8
Stanislav Atanasov <lucifer@anavaro.com>
2015-02-11 22:18:11 +0200

    Change user test as we miss admin update

--- tests/functional/phpbbgallery_alpha_test.php
+++ tests/functional/phpbbgallery_alpha_test.php
@@ -332,7 +332,7 @@ class phpbbgallery_alpha_test extends phpbbgallery_base
 		
 		$crawler = self::request('GET', 'app.php/gallery/album/1');
 		
-		$this->assertContains('1 image',  $crawler->text());
+		$this->assertContains('0 image',  $crawler->text());
 		$this->assertContains('Valid',  $crawler->text());
 		
 		$this->logout();
