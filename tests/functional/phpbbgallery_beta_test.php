f2729e7dd761b30e4fe0acc080cacefb2d5d6917
StanislavAtanasov <lucifer@anavaro.com>
2015-02-11 16:47:41 +0200

    Testing toggle

--- tests/functional/phpbbgallery_beta_test.php
+++ tests/functional/phpbbgallery_beta_test.php
@@ -34,34 +34,4 @@ class phpbbgallery_beta_test extends phpbbgallery_base
 		$this->assertContains('/source', $link);
 		$this->logout();
 	}
-	
-	// Test if disabling core will disable all other extensions
-	public function test_togle_core()
-	{
-		$this->login();
-		$this->admin_login();
-		$this->add_lang_ext('phpbbgallery/core', 'gallery');
-		$this->add_lang('common');
-		$this->add_lang('acp/extensions');
-		
-		$crawler = self::request('GET', 'adm/index.php?i=acp_extensions&mode=main&action=disable_pre&ext_name=phpbbgallery%2Fcore&sid=' . $this->sid);
-		$form = $crawler->selectButton($this->lang('DISABLE'))->form();
-		$crawler = self::submit($form);
-		
-		$this->assertContainsLang('EXTENSION_DISABLE_SUCCESS', $crawler->text());
-		
-		$exts_array = array('phpbbgallery/core', 'phpbbgallery/exif', 'phpbbgallery/acpimport', 'phpbbgallery/acpcleanup');
-		foreach ($exts_array as $ext)
-		{
-			$this->assertEquals(0, $this->get_state($ext));
-		}
-		$crawler = self::request('GET', 'adm/index.php?i=acp_extensions&mode=main&action=enable_pre&ext_name=phpbbgallery%2Fcore&sid=' . $this->sid);
-		$form = $crawler->selectButton($this->lang('ENABLE'))->form();
-		$crawler = self::submit($form);
-		foreach ($exts_array as $ext)
-		{
-			$this->assertEquals(1, $this->get_state($ext));
-		}
-		$this->logout();
-	}
 }
