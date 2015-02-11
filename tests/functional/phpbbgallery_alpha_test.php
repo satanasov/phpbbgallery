c6a4546d6737f3e8f9abe4fa9a9eddb224f89cee
Stanislav Atanasov <lucifer@anavaro.com>
2015-02-11 13:56:19 +0200

    TEST ACP Install TEST ACP Modules active

--- tests/functional/phpbbgallery_alpha_test.php
+++ tests/functional/phpbbgallery_alpha_test.php
@@ -21,45 +21,57 @@ class phpbbgallery_alpha_test extends phpbbgallery_base
 			'core_verview'	=> array(
 				'phpbbgallery/core',
 				'gallery_acp',
-				'adm/index.php?i=-phpbbgallery-core-acp-main_module&mode=overview'
+				'adm/index.php?i=-phpbbgallery-core-acp-main_module&mode=overview',
 				$this->user->lang('ACP_GALLERY_OVERVIEW_EXPLAIN')
 			),
 			'core_config'	=> array(
 				'phpbbgallery/core',
 				'gallery_acp',
-				'adm/index.php?i=-phpbbgallery-core-acp-config_module&mode=main'
+				'adm/index.php?i=-phpbbgallery-core-acp-config_module&mode=main',
 				$this->user->lang('GALLERY_CONFIG')
 			),
 			'core_albums'	=> array(
 				'phpbbgallery/core',
 				'gallery_acp',
-				'adm/index.php?i=-phpbbgallery-core-acp-albums_module&mode=manage'
+				'adm/index.php?i=-phpbbgallery-core-acp-albums_module&mode=manage',
 				$this->user->lang('ALBUM_ADMIN')
 			),
 			'core_perms'	=> array(
 				'phpbbgallery/core',
 				'gallery_acp',
-				'adm/index.php?i=-phpbbgallery-core-acp-permissions_module&mode=manage'
+				'adm/index.php?i=-phpbbgallery-core-acp-permissions_module&mode=manage',
 				$this->user->lang('PERMISSIONS_EXPLAIN')
 			),
 			'core_copy_perms'	=> array(
 				'phpbbgallery/core',
 				'gallery_acp',
-				'adm/index.php?i=-phpbbgallery-core-acp-permissions_module&mode=copy'
+				'adm/index.php?i=-phpbbgallery-core-acp-permissions_module&mode=copy',
 				$this->user->lang('PERMISSIONS_COPY')
 			),
 			'core_log'	=> array(
 				'phpbbgallery/core',
 				'info_acp_gallery_logs',
-				'adm/index.php?i=-phpbbgallery-core-acp-gallery_logs_module&mode=main'
+				'adm/index.php?i=-phpbbgallery-core-acp-gallery_logs_module&mode=main',
 				$this->user->lang('LOG_GALLERY_SHOW_LOGS')
 			),
 			// This is core, now extensions
 			'exif'	=> array(
 				'phpbbgallery/exif',
 				'exif',
-				'adm/index.php?i=-phpbbgallery-core-acp-config_module&mode=main'
+				'adm/index.php?i=-phpbbgallery-core-acp-config_module&mode=main',
 				$this->user->lang('DISP_EXIF_DATA')
+			),
+			'acp_cleanup'	=> array(
+				'phpbbgallery/acpcleanup',
+				'info_acp_gallery_cleanup',
+				'adm/index.php?i=-phpbbgallery-acpcleanup-acp-main_module&mode=cleanup',
+				$this->user->lang('ACP_GALLERY_CLEANUP')
+			),
+			'acp_import'	=> array(
+				'phpbbgallery/acpimport',
+				'info_acp_gallery_cleanup',
+				'adm/index.php?i=-phpbbgallery-acpimport-acp-main_module&mode=import_images',
+				$this->user->lang('ACP_IMPORT_ALBUMS')
 			),
 		);
 	}
