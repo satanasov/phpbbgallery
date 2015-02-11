7755b7989437d0218fd2745095c216ae8b01dd8b
StanislavAtanasov <lucifer@anavaro.com>
2015-02-11 21:59:30 +0200

    Some duplicated tests

--- tests/functional/phpbbgallery_beta_test.php
+++ tests/functional/phpbbgallery_beta_test.php
@@ -66,42 +66,4 @@ class phpbbgallery_beta_test extends phpbbgallery_base
 		}
 		$this->logout();
 	}
-	// Create album for testing
-	public function test_admin_create_album()
-	{
-		$this->login();
-		$this->admin_login();
-		
-		// Let us create a user we will use for tests
-		$this->create_user('testuser1');
-		$this->add_user_group('REGISTERED', array('testuser1'));
-		// Let me get admin out of registered
-		$this->remove_user_group('REGISTERED', array('admin'));
-		
-		$this->add_lang_ext('phpbbgallery/core', 'gallery_acp');
-		$crawler = self::request('GET', 'adm/index.php?i=-phpbbgallery-core-acp-albums_module&mode=manage&sid=' . $this->sid);
-		
-		// Step 1
-		$form = $crawler->selectButton($this->lang('CREATE_ALBUM'))->form();
-		$form['album_name'] = 'First test album!';
-		$crawler = self::submit($form);
-		
-		// Step 2 - we should have reached a form for creating album_name
-		$this->assertContainsLang('ALBUM_EDIT_EXPLAIN', $crawler->text());
-		
-		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
-		$crawler = self::submit($form);
-		
-		// Step 3 - Album should be created and we should have option to add permissions
-		$this->assertContainsLang('ALBUM_CREATED', $crawler->text());
-		
-		$crawler = self::request('GET', 'adm/index.php?i=-phpbbgallery-core-acp-albums_module&mode=manage&sid=' . $this->sid);
-		$this->assertContains('First test album!', $crawler->text());
-		
-		$crawler = self::request('GET', 'app.php/gallery');
-		$this->assertContains('First test album!', $crawler->text());
-		
-		$this->logout();
-		$this->logout();
-	}
 }
