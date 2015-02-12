b3dcb70bb66ee2d61938c8e0d14b9df3ef55a1b7
StanislavAtanasov <lucifer@anavaro.com>
2015-02-12 17:54:30 +0200

    Add some languge to translation

--- core/language/en/permissions_gallery.php
+++ core/language/en/permissions_gallery.php
@@ -23,12 +23,8 @@ if (empty($lang) || !is_array($lang))
 	$lang = array();
 }
 
-$lang['permission_cat']['gallery'] = 'phpBB Gallery';
-
 // Adding the permissions
 $lang = array_merge($lang, array(
-	'acl_a_gallery_manage'		=> array('lang' => 'Can manage the phpBB Gallery adjustments',	'cat' => 'gallery'),
-	'acl_a_gallery_albums'		=> array('lang' => 'Can add/edit albums and permissions',		'cat' => 'gallery'),
-	'acl_a_gallery_import'		=> array('lang' => 'Can use the import-function',				'cat' => 'gallery'),
-	'acl_a_gallery_cleanup'		=> array('lang' => 'Can clean up the phpBB Gallery',			'cat' => 'gallery'),
+	'ACL_A_GALLERY_MANAGE'		=> 'Can manage the phpBB Gallery adjustments',
+	'ACL_A_GALLERY_ALBUMS'		=> 'Can add/edit albums and permissions',
 ));
