eded7883503b96e0120de7d73926891e46d8ad27
Stanislav Atanasov <lucifer@anavaro.com>
2015-02-13 01:40:21 +0200

    Add test for image name

--- tests/functional/phpbbgallery_beta_test.php
+++ tests/functional/phpbbgallery_beta_test.php
@@ -74,12 +74,12 @@ class phpbbgallery_beta_test extends phpbbgallery_base
 			'image'	=> array(
 				'image',
 				true,
-				'app.php/gallery/image/1/source'
+				'app.php/gallery/image/2/source'
 			),
 			'image_page'	=> array(
 				'image_page',
 				true,
-				'app.php/gallery/image/1'
+				'app.php/gallery/image/2'
 			),
 			'none'	=> array(
 				'none',
@@ -119,6 +119,40 @@ class phpbbgallery_beta_test extends phpbbgallery_base
 		{
 			$this->assertEquals(0, $object->filter('a')->count());
 			$this->assertEquals(1, $object->filter('img')->count());
+		}
+		
+		$this->logout();
+	}
+	/**
+	* @dataProvider thumbnail_link_data
+	*/
+	public function test_thumbnail_link($option, $has_link, $search)
+	{
+		$this->login();
+		$this->admin_login();
+		$this->add_lang_ext('phpbbgallery/core', 'gallery');
+		$this->add_lang_ext('phpbbgallery/core', 'gallery_acp');
+		$this->add_lang('common');
+		
+		// Change option
+		$crawler = self::request('GET', 'adm/index.php?i=-phpbbgallery-core-acp-config_module&mode=main&sid=' . $this->sid);
+		$form = $crawler->selectButton('submit')->form();
+		$form->setValues(array(
+			'config[link_image_name]'	=> $option,
+		));
+		$crawler = self::submit($form);
+		// Should be updated
+		$this->assertContainsLang('GALLERY_CONFIG_UPDATED', $crawler->text());
+
+		$crawler = self::request('GET', 'app.php/gallery/album/1');
+		$object = $crawler->filter('div.polaroid')->eq(1)->filter('p')->eq(0);
+		if ($has_link)
+		{
+			$this->assertContains($search, $object->filter('a')->attr('href'));
+		}
+		else
+		{
+			$this->assertContains('', $object->filter('a')->attr('href'));
 		}
 		
 		$this->logout();
