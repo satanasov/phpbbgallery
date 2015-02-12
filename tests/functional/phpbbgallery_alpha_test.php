c40aa33f78129f844599c8015f55319ceb8b925c
StanislavAtanasov <lucifer@anavaro.com>
2015-02-12 22:42:33 +0200

    test thumbanaul

--- tests/functional/phpbbgallery_alpha_test.php
+++ tests/functional/phpbbgallery_alpha_test.php
@@ -889,9 +889,7 @@ class phpbbgallery_alpha_test extends phpbbgallery_base
 		}
 		else
 		{
-			//$this->assertEquals(0, $crawler->filter('div#image')->filter('a')->count());
-			$link = $crawler->filter('div#image')->filter('a')->attr('href');
-			
+			$this->assertEquals(0, $crawler->filter('div#image')->filter('a')->count());
 			$this->assertEquals(1, $crawler->filter('div#image')->filter('img')->count());
 		}		
 		$this->logout();
