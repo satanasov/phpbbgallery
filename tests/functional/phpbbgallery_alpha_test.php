6050bbb97a50abaf03bdd11bff41b63df9ea416d
Stanislav Atanasov <lucifer@anavaro.com>
2015-02-13 06:36:59 +0200

    test 1234

--- tests/functional/phpbbgallery_alpha_test.php
+++ tests/functional/phpbbgallery_alpha_test.php
@@ -819,7 +819,7 @@ class phpbbgallery_alpha_test extends phpbbgallery_base
 		
 		$crawler = self::request('GET', 'app.php/gallery/album/1');
 		
-		$this->assertContains('zazazazazazazazaza', $crawler->filter('div.polaroid > a:contains("First test album")')->text());
+		$this->assertContains('zazazazazazazazaza', $crawler->filter('div.polaroid')->eq(0)->text());
 		$link = $crawler->filter('div.polaroid > a:contains("First test album")')->attr('href');
 		$crawler = self::request('GET', $link);
 		
