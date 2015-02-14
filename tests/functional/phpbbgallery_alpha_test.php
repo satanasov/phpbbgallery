b68002ced8209fda66292b9e398f5fbff3867af6
StanislavAtanasov <lucifer@anavaro.com>
2015-02-14 05:07:34 +0200

    typo

--- tests/functional/phpbbgallery_alpha_test.php
+++ tests/functional/phpbbgallery_alpha_test.php
@@ -925,7 +925,7 @@ class phpbbgallery_alpha_test extends phpbbgallery_base
 		
 		// Step 1 - see subalbums
 		$url = $crawler->filter('a:contains("First test album!")')->attr('href');
-		$crawler = self::request('GET', s$url);
+		$crawler = self::request('GET', $url);
 		
 		$url = $crawler->filter('a:contains("First sub test album!")')->parents()->parents()->filter('td')->eq(2)->filter('a')->eq(3)->attr('href');
 		$this->assertContains('zazazzza', $url);
