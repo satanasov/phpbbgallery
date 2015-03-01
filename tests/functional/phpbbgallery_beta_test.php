5b22d19b8c8fd30e31e62ef63ae0f5ed88aaef4c
Stanislav Atanasov <lucifer@anavaro.com>
2015-03-01 20:06:52 +0200

    Form issues

--- tests/functional/phpbbgallery_beta_test.php
+++ tests/functional/phpbbgallery_beta_test.php
@@ -193,7 +193,7 @@ class phpbbgallery_beta_test extends phpbbgallery_base
 
 		$crawler = self::request('GET', substr($url, 1));
 		
-		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
+		$form = $crawler->selectButton('submit')->eq(1)->form();
 
 		$crawler = self::submit($form);
 		
