5c7a7734274ba246041612def68aab934e1611a1
Stanislav Atanasov <lucifer@anavaro.com>
2015-03-04 14:24:26 +0200

    Init CPFs (or we will not have a request for them)

--- tests/functional/phpbbgallery_charlie_test.php
+++ tests/functional/phpbbgallery_charlie_test.php
@@ -13,6 +13,36 @@ namespace phpbbgallery\tests\functional;
 */
 class phpbbgallery_charlie_test extends phpbbgallery_base
 {
+	//We need to init the cpfs
+	public function test_init_cpfs()
+	{
+		$this->add_lang_ext('phpbbgallery/core', 'gallery');
+		$this->add_lang_ext('phpbbgallery/core', 'gallery_ucp');
+		$this->add_lang('ucp');
+
+		$this->login();
+		$crawler = self::request('GET', 'ucp.php?i=ucp_profile&mode=profile_info&sid=' . $this->sid);
+		$form = $crawler->selectButton('submit')->form();
+		$form->setValues(array(
+			'pf_phpbb_location'	=> 'test',
+		));
+		$crawler = self::submit($form);
+		// Should be updated
+		$this->assertContainsLang('PROFILE_UPDATED', $crawler->text());
+		$this->logout();
+		
+		//testuser1
+		$this->login('testuser1');
+		$crawler = self::request('GET', 'ucp.php?i=ucp_profile&mode=profile_info&sid=' . $this->sid);
+		$form = $crawler->selectButton('submit')->form();
+		$form->setValues(array(
+			'pf_phpbb_location'	=> 'test',
+		));
+		$crawler = self::submit($form);
+		// Should be updated
+		$this->assertContainsLang('PROFILE_UPDATED', $crawler->text());
+		$this->logout();
+	}
 	public function show_link_data()
 	{
 		return array(
@@ -42,7 +72,6 @@ class phpbbgallery_charlie_test extends phpbbgallery_base
 		
 		$this->set_option('profile_pega', $state);
 		$crawler = self::request('GET', 'memberlist.php?mode=viewprofile&u=' . $this->get_user_id('admin') . '&sid=' . $this->sid);
-		$this->assertContains('zazazaza', $crawler->text());
 		$this->assertEquals($count, $crawler->filter('a:contains("Visit user Gallery")')->count());
 		
 		$crawler = self::request('GET', 'memberlist.php?mode=viewprofile&u=' . $this->get_user_id('testuser1') . '&sid=' . $this->sid);
