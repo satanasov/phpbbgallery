4b0e54e88b7a61d551b17aaa6d60a4bf2bb77b25
Stanislav Atanasov <lucifer@anavaro.com>
2015-03-06 19:51:45 +0200

    Fix typo in Alpha tests Add Delta test for extensions Add Test to copy to import gallery

---
+++ tests/functional/phpbbgallery_delta_test.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* 
+* Gallery Control test
+*
+* @copyright (c) 2014 Stanislav Atanasov
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+namespace phpbbgallery\tests\functional;
+/**
+* @group functional
+*/
+class phpbbgallery_delta_test extends phpbbgallery_base
+{
+	public function test_prepare_import()
+	{
+		$this->asserEquals(1, copy(__DIR__ . '/images/valid.jpg', __DIR__ . '/../../../files/core/import/1.jpg'));
+	}
+}
