2d9751551f0d93aca5e88e4eb626ffff15a2f366
StanislavAtanasov <lucifer@anavaro.com>
2015-04-07 00:42:05 +0300

    add basic test support for zipunload returns errors

--- tests/functional/phpbbgallery_beta_test.php
+++ tests/functional/phpbbgallery_beta_test.php
@@ -1746,6 +1746,51 @@ class phpbbgallery_beta_test extends phpbbgallery_base
 		$this->logout();
 		$this->logout();
 	}
+	/**
+	* @dataProvider yes_no_data
+	*/
+	/*public function test_allow_zip($option)
+	{
+		$this->login();
+		$this->admin_login();
+		$this->add_lang_ext('phpbbgallery/core', 'gallery');
+		$this->add_lang_ext('phpbbgallery/core', 'gallery_acp');
+		$this->add_lang('common');
+		
+		// Change option
+		$crawler = self::request('GET', 'adm/index.php?i=-phpbbgallery-core-acp-config_module&mode=main&sid=' . $this->sid);
+		$form = $crawler->selectButton('submit')->form();
+		$form->setValues(array(
+			'config[allow_zip]'	=> $option,
+		));
+		$crawler = self::submit($form);
+		// Should be updated
+		$this->assertContainsLang('GALLERY_CONFIG_UPDATED', $crawler->text());
+		
+		// Test
+		$crawler = self::request('GET', 'app.php/gallery/album/1');
+		$upload_url = substr($crawler->filter('a:contains("' . $this->lang('UPLOAD_IMAGE') . '")')->attr('href'), 1);
+		$crawler = self::request('GET', $upload_url);
+		$form = $crawler->selectButton($this->lang('CONTINUE'))->form();
+		$form['image_file_0'] =  __DIR__ . '/images/valid.zip';
+		$crawler = self::submit($form);
+		if ($option == 1)
+		{
+			$form = $crawler->selectButton($this->lang['SUBMIT'])->form();
+			$form['image_name'] = array(
+				0 => 'Test zip image',
+			);
+			$crawler = self::submit($form);
+			
+			$this->assertContainsLang('ALBUM_UPLOAD_SUCCESSFUL', $crawler->text());
+		}
+		else
+		{
+			$this->assertContains($this->lang('DISALLOWED_EXTENSION'), $crawler->filter('p.error')->text());
+		}
+		$this->logout();
+		$this->logout();
+	}/*
 	// END IMAGE SETTINGS
 	/**
 	* @dataProvider image_on_image_page_data
