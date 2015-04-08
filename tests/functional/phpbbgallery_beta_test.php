ed06dbfd67cc80b4b5400df400fe46624258a2ff
Stanislav Atanasov <lucifer@anavaro.com>
2015-04-09 02:21:32 +0300

    Test should work

--- tests/functional/phpbbgallery_beta_test.php
+++ tests/functional/phpbbgallery_beta_test.php
@@ -2193,7 +2193,7 @@ class phpbbgallery_beta_test extends phpbbgallery_base
 		$crawler = self::request('GET', 'adm/index.php?i=-phpbbgallery-core-acp-config_module&mode=main&sid=' . $this->sid);
 		$form = $crawler->selectButton('submit')->form();
 		$form->setValues(array(
-			'config[rrc_gindex_mode]'	=> $options,
+			'rrc_gindex_mode'	=> $options,
 		));
 		$crawler = self::submit($form);
 		// Should be updated
@@ -2228,7 +2228,7 @@ class phpbbgallery_beta_test extends phpbbgallery_base
 		$crawler = self::request('GET', 'adm/index.php?i=-phpbbgallery-core-acp-config_module&mode=main&sid=' . $this->sid);
 		$form = $crawler->selectButton('submit')->form();
 		$form->setValues(array(
-			'rrc_gindex_comments'	=> $options,
+			'config[rrc_gindex_comments]'	=> $option,
 		));
 		$crawler = self::submit($form);
 		// Should be updated
