a02469057852d3697715744337d41b505e81cf7f
Stanislav Atanasov <lucifer@anavaro.com>
2015-08-01 16:51:58 +0300

    Fix issue where gallery breaks if parrent album or cat has no ACL

--- core/album/display.php
+++ core/album/display.php
@@ -500,7 +500,7 @@ class display
 				$album_rows[$parent_id]['album_contest_marked'] = $row['contest_marked'];
 				$album_rows[$parent_id]['orig_album_last_image_time'] = $row['album_last_image_time'];
 			}
-			else if ($row['album_type'])
+			else if ($row['album_type'] && isset($subalbums[$parent_id]))
 			{
 				$subalbums[$parent_id][$album_id]['display'] = ($row['display_on_index']) ? true : false;
 				$subalbums[$parent_id][$album_id]['name'] = $row['album_name'];
