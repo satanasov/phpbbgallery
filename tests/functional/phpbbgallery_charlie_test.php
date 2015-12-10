12b838ef210583eb50407a87de66275db90291b1
Stanislav Atanasov <lucifer@anavaro.com>
2015-12-10 04:39:34 +0200

    Make event tests Remove link in profile test from functional and add to event

--- tests/functional/phpbbgallery_charlie_test.php
+++ tests/functional/phpbbgallery_charlie_test.php
@@ -43,38 +43,4 @@ class phpbbgallery_charlie_test extends phpbbgallery_base
 		$this->assertContainsLang('PROFILE_UPDATED', $crawler->text());
 		$this->logout();
 	}
-	public function show_link_data()
-	{
-		return array(
-			'yes'	=> array(
-				1,
-				1
-			),
-			'no'	=> array(
-				0,
-				0
-			),
-			'reset'	=> array(
-				1,
-				1
-			),
-		);
-	}
-	/**
-	* @dataProvider show_link_data
-	*/
-	public function test_contact_link($state, $count)
-	{
-		$this->login();
-		$this->add_lang_ext('phpbbgallery/core', 'gallery');
-		$this->add_lang_ext('phpbbgallery/core', 'gallery_ucp');
-		$this->add_lang('ucp');
-		
-		$this->set_option('profile_pega', $state);
-		$crawler = self::request('GET', 'memberlist.php?mode=viewprofile&u=' . $this->get_user_id('admin') . '&sid=' . $this->sid);
-		$this->assertEquals($count, $crawler->filter('a:contains("' . $this->lang('VISIT_GALLERY') . '")')->count());
-		
-		$crawler = self::request('GET', 'memberlist.php?mode=viewprofile&u=' . $this->get_user_id('testuser1') . '&sid=' . $this->sid);
-		$this->assertEquals(0, $crawler->filter('a:contains("' . $this->lang('VISIT_GALLERY') . '")')->count());
-	}
 }
