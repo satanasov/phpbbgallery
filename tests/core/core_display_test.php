35a4c8bf3dcf420f3b7f93f16eee0aca3250bc2c
Stanislav Atanasov <lucifer@anavaro.com>
2015-12-14 03:15:08 +0200

    Begin \core\album\display test Begin \core\controller\index -> base test (2 cases)

---
+++ tests/core/core_display_test.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+*
+* PhpBB Gallery extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2015 Lucifer <https://www.anavaro.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace phpbbgallery\tests\core;
+/**
+* @group core2
+*/
+require_once dirname(__FILE__) . '/../../../../includes/functions.php';
+
+class core_display_test extends core_base
+{
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->gallery_cache = new \phpbbgallery\core\cache(
+			$this->cache,
+			$this->db,
+			'phpbb_gallery_albums',
+			'phpbb_gallery_images'
+		);
+		
+		$this->gallery_user = new \phpbbgallery\core\user(
+			$this->db,
+			$this->dispatcher,
+			'phpbb_gallery_users'
+		);
+		
+		// Let's build auth class
+		$this->gallery_auth = new \phpbbgallery\core\auth\auth(
+			$this->gallery_cache,
+			$this->db,
+			$this->gallery_user,
+			$this->user,
+			$this->auth,
+			'phpbb_gallery_permissions',
+			'phpbb_gallery_roles',
+			'phpbb_gallery_users',
+			'phpbb_gallery_albums'
+		);
+		
+		$this->misc = $this->getMockBuilder('\phpbbgallery\core\misc')
+			->disableOriginalConstructor()
+			->getMock();
+		
+		$this->display = new \phpbbgallery\core\album\display(
+			$this->auth,
+			$this->config,
+			$this->controller_helper,
+			$this->db,
+			$this->pagination,
+			$this->request,
+			$this->template,
+			$this->user,
+			$this->gallery_auth,
+			$this->gallery_user,
+			$this->misc,
+			'/',
+			'php',
+			'phpbb_gallery_albums',
+			'phpbb_gallery_contests',
+			'phpbb_gallery_albums_track',
+			'phpbb_gallery_albums_modscache'
+		);
+	}
+
+	public function test_get_branch()
+	{
+		$this->assertTrue(true);
+	}
+}
