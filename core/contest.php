58d2b00f1ead82211302cb83a0c51986935f0be6
StanislavAtanasov <lucifer@anavaro.com>
2016-04-03 17:11:23 +0300

    This should fix validation issues for \phpbbgallery\core\contest ### Contests are not implemented!

--- core/contest.php
+++ core/contest.php
@@ -33,6 +33,17 @@ class contest
 	*/
 	const MODE_SUM = 2;
 
+	public function __construct(\phpbb\db\driver\driver_interface $db,
+								\phpbbgallery\core\image\image $image, \phpbbgallery\core\config $gallery_config,
+								$images_table, $contests_table)
+	{
+		$this->db = $db;
+		$this->image = $image;
+		$this->gallery_config = $gallery_config;
+		$this->images_table = $images_table;
+		$this->contest_table = $contests_table;
+	}
+
 	/**
 	* Get the contest row from the table
 	*
@@ -42,17 +53,14 @@ class contest
 	*
 	* @return	mixed	Either the array or boolean false if contest does not exist
 	*/
-	function get_contest($id, $mode = 'contest', $throw_error = true)
+	public function get_contest($id, $mode = 'contest', $throw_error = true)
 	{
-		global $db, $table_prefix;
-		$contests_table = $table_prefix . 'gallery_contests';
-
 		$sql = 'SELECT *
-			FROM ' . $contests_table . '
+			FROM ' . $this->contest_table . '
 			WHERE ' . (($mode = 'album') ? 'contest_album_id' : 'contest_id') . ' = ' . (int) $id;
-		$result = $db->sql_query_limit($sql, 1);
-		$row = $db->sql_fetchrow($result);
-		$db->sql_freeresult($result);
+		$result = $this->db->sql_query_limit($sql, 1);
+		$row = $this->db->sql_fetchrow($result);
+		$this->db->sql_freeresult($result);
 
 		if (!$row && $throw_error)
 		{
@@ -62,7 +70,7 @@ class contest
 		return (!$row) ? false : $row;
 	}
 
-	static private function get_tabulation()
+	private function get_tabulation()
 	{
 		switch (self::$mode)
 		{
@@ -73,7 +81,7 @@ class contest
 		}
 	}
 
-	static public function is_step($mode, $album_data)
+	public function is_step($mode, $album_data)
 	{
 		switch ($mode)
 		{
@@ -88,109 +96,105 @@ class contest
 		}
 	}
 
-	static public function end($album_id, $contest_id, $end_time)
+	public function end($album_id, $contest_id, $end_time)
 	{
-		global $db;
-
-		$sql = 'UPDATE ' . GALLERY_IMAGES_TABLE . '
-			SET image_contest = ' . phpbb_gallery_image::NO_CONTEST . '
-			WHERE image_album_id = ' . $album_id;
-		$db->sql_query($sql);
+		$sql = 'UPDATE ' . $this->images_table . '
+			SET image_contest = ' . $this->image->get_no_contest() . '
+			WHERE image_album_id = ' . (int) $album_id;
+		$this->db->sql_query($sql);
 
 		$sql = 'SELECT image_id
-			FROM ' . GALLERY_IMAGES_TABLE . '
-			WHERE image_album_id = ' . $album_id . '
-			ORDER BY ' . self::get_tabulation();
-		$result = $db->sql_query_limit($sql, self::NUM_IMAGES);
-		$first = (int) $db->sql_fetchfield('image_id');
-		$second = (int) $db->sql_fetchfield('image_id');
-		$third = (int) $db->sql_fetchfield('image_id');
-		$db->sql_freeresult($result);
+			FROM ' . $this->images_table . '
+			WHERE image_album_id = ' . (int) $album_id . '
+			ORDER BY ' .$this->get_tabulation();
+		$result = $this->db->sql_query_limit($sql, self::NUM_IMAGES);
+		$first = (int) $this->db->sql_fetchfield('image_id');
+		$second = (int) $this->db->sql_fetchfield('image_id');
+		$third = (int) $this->db->sql_fetchfield('image_id');
+		$this->db->sql_freeresult($result);
 
-		$sql = 'UPDATE ' . GALLERY_CONTESTS_TABLE . '
-			SET contest_marked = ' . phpbb_gallery_image::NO_CONTEST . ",
+		$sql = 'UPDATE ' . $this->contest_table . '
+			SET contest_marked = ' . $this->image->get_no_contest() . ",
 				contest_first = $first,
 				contest_second = $second,
 				contest_third = $third
 			WHERE contest_id = " . (int) $contest_id;
-		$db->sql_query($sql);
+		$this->db->sql_query($sql);
 
-		$sql = 'UPDATE ' . GALLERY_IMAGES_TABLE . '
+		$sql = 'UPDATE ' . $this->images_table . '
 			SET image_contest_end = ' . (int) $end_time . ',
 				image_contest_rank = 1
 			WHERE image_id = ' . $first;
-		$db->sql_query($sql);
+		$this->db->sql_query($sql);
 
-		$sql = 'UPDATE ' . GALLERY_IMAGES_TABLE . '
+		$sql = 'UPDATE ' . $this->images_table . '
 			SET image_contest_end = ' . (int) $end_time . ',
 				image_contest_rank = 2
 			WHERE image_id = ' . $second;
-		$db->sql_query($sql);
+		$this->db->sql_query($sql);
 
-		$sql = 'UPDATE ' . GALLERY_IMAGES_TABLE . '
+		$sql = 'UPDATE ' . $this->images_table . '
 			SET image_contest_end = ' . (int) $end_time . ',
 				image_contest_rank = 3
 			WHERE image_id = ' . $third;
-		$db->sql_query($sql);
+		$this->db->sql_query($sql);
 
-		phpbb_gallery_config::inc('contests_ended', 1);
+		$this->gallery_config->inc('contests_ended', 1);
 	}
 
-	static public function resync_albums($album_ids)
+	public function resync_albums($album_ids)
 	{
 		if (is_array($album_ids))
 		{
 			$album_ids = array_map('intval', $album_ids);
 			foreach ($album_ids as $album_id)
 			{
-				self::resync($album_id);
+				$this->resync($album_id);
 			}
 		}
 		else
 		{
-			self::resync((int) $album_ids);
+			$this->resync((int) $album_ids);
 		}
 	}
 
-	static public function resync($album_id)
+	public function resync($album_id)
 	{
-		global $db;
-
-		$sql = 'UPDATE ' . GALLERY_IMAGES_TABLE . '
-			SET image_contest = ' . phpbb_gallery_image::NO_CONTEST . '
-			WHERE image_album_id = ' . $album_id;
-		$db->sql_query($sql);
+		$sql = 'UPDATE ' . $this->images_table . '
+			SET image_contest = ' . $this->image->get_no_contest() . '
+			WHERE image_album_id = ' . (int) $album_id;
+		$this->db->sql_query($sql);
 
 		$sql = 'SELECT image_id
-			FROM ' . GALLERY_IMAGES_TABLE . '
-			WHERE image_album_id = ' . $album_id . '
-			ORDER BY ' . self::get_tabulation();
-		$result = $db->sql_query_limit($sql, self::NUM_IMAGES);
-		$first = (int) $db->sql_fetchfield('image_id');
-		$second = (int) $db->sql_fetchfield('image_id');
-		$third = (int) $db->sql_fetchfield('image_id');
-		$db->sql_freeresult($result);
+			FROM ' . $this->images_table . '
+			WHERE image_album_id = ' . (int) $album_id . '
+			ORDER BY ' . $this->get_tabulation();
+		$result = $this->db->sql_query_limit($sql, self::NUM_IMAGES);
+		$first = (int) $this->db->sql_fetchfield('image_id');
+		$second = (int) $this->db->sql_fetchfield('image_id');
+		$third = (int) $this->db->sql_fetchfield('image_id');
+		$this->db->sql_freeresult($result);
 
-		$sql = 'UPDATE ' . GALLERY_CONTESTS_TABLE . "
+		$sql = 'UPDATE ' . $this->contest_table . "
 			SET contest_first = $first,
 				contest_second = $second,
 				contest_third = $third
 			WHERE contest_album_id = " . (int) $album_id;
-		$db->sql_query($sql);
+		$this->db->sql_query($sql);
 
-		$sql = 'UPDATE ' . GALLERY_IMAGES_TABLE . '
+		$sql = 'UPDATE ' . $this->images_table . '
 			SET image_contest_rank = 1
 			WHERE image_id = ' . $first;
-		$db->sql_query($sql);
+		$this->db->sql_query($sql);
 
-		$sql = 'UPDATE ' . GALLERY_IMAGES_TABLE . '
+		$sql = 'UPDATE ' . $this->images_table . '
 			SET image_contest_rank = 2
 			WHERE image_id = ' . $second;
-		$db->sql_query($sql);
+		$this->db->sql_query($sql);
 
-		$sql = 'UPDATE ' . GALLERY_IMAGES_TABLE . '
+		$sql = 'UPDATE ' . $this->images_table . '
 			SET image_contest_rank = 3
 			WHERE image_id = ' . $third;
-		$db->sql_query($sql);
+		$this->db->sql_query($sql);
 	}
 }
