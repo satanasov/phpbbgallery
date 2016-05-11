5d4ff01f3135aba43884dfeacdbf0277abb4a89c
Stanislav Atanasov <lucifer@anavaro.com>
2016-05-12 00:16:32 +0300

    Fixing tests after moving some constants to block

--- core/misc.php
+++ core/misc.php
@@ -44,7 +44,7 @@ class misc
 		{
 			return $gallery_display_captcha[$mode];
 		}
-var_dump('Dragons');
+
 		$gallery_display_captcha[$mode] = ($this->user->data['user_id'] == ANONYMOUS) && $this->gallery_config->get('captcha_' . $mode);
 
 		return $gallery_display_captcha[$mode];
