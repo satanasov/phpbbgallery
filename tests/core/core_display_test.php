06c3e86084194fdb016fc096a966ee6e4f26c36e
Stanislav Atanasov <lucifer@anavaro.com>
2016-05-16 03:28:15 +0300

    Add partial test for \phpbbgallery\core\album\display

--- tests/core/core_display_test.php
+++ tests/core/core_display_test.php
@@ -11,7 +11,7 @@
 
 namespace phpbbgallery\tests\core;
 /**
-* @group core2
+* @group core1
 */
 require_once dirname(__FILE__) . '/../../../../includes/functions.php';
 
@@ -27,7 +27,7 @@ class core_display_test extends core_base
 			'phpbb_gallery_albums',
 			'phpbb_gallery_images'
 		);
-		
+
 		$this->gallery_user = new \phpbbgallery\core\user(
 			$this->db,
 			$this->dispatcher,
@@ -38,7 +38,7 @@ class core_display_test extends core_base
 			'/',
 			'php'
 		);
-		
+
 		// Let's build auth class
 		$this->gallery_auth = new \phpbbgallery\core\auth\auth(
 			$this->gallery_cache,
@@ -51,11 +51,11 @@ class core_display_test extends core_base
 			'phpbb_gallery_users',
 			'phpbb_gallery_albums'
 		);
-		
+
 		$this->misc = $this->getMockBuilder('\phpbbgallery\core\misc')
 			->disableOriginalConstructor()
 			->getMock();
-		
+
 		$this->display = new \phpbbgallery\core\album\display(
 			$this->auth,
 			$this->config,
@@ -76,8 +76,226 @@ class core_display_test extends core_base
 			'phpbb_gallery_albums_modscache'
 		);
 	}
-	public function test_get_branch()
+	/**
+	 * Test data for the test_get_branch test
+	 *
+	 * @return array Test data
+	 */
+	public function get_branch_data()
 	{
-		$this->assertTrue(true);
+		return array(
+			'album1_default'	=> array(
+				0, // branch user id
+				1, // album_id,
+				'all', // Type
+				'descending', // Order
+				true, // Include album
+				array( // expected
+					0 => Array (
+						'album_id' => '1',
+						'parent_id' => '0',
+						'left_id' => '0',
+						'right_id' => '0',
+						'album_parents' => '',
+						'album_type' => '1',
+						'album_status' => '0',
+						'album_contest' => '0',
+						'album_name' => 'TestPublicAlbum1',
+						'album_desc' => '',
+						'album_desc_options' => '7',
+						'album_desc_uid' => '',
+						'album_desc_bitfield' => '',
+						'album_user_id' => '0',
+						'album_images' => '3',
+						'album_images_real' => '3',
+						'album_last_image_id' => '3',
+						'album_image' => '',
+						'album_last_image_time' => '0',
+						'album_last_image_name' => 'TestImage3',
+						'album_last_username' => 'admin',
+						'album_last_user_colour' => '',
+						'album_last_user_id' => '2',
+						'album_watermark' => '1',
+						'album_sort_key' => '',
+						'album_sort_dir' => '',
+						'display_in_rrc' => '1',
+						'display_on_index' => '1',
+						'display_subalbum_list' => '1',
+						'album_feed' => '1',
+						'album_auth_access' => '0',
+					)
+				)
+			),
+			'album1_children'	=> array(
+				0, // branch user id
+				1, // album_id,
+				'children', // Type
+				'descending', // Order
+				true, // Include album
+				array( // expected
+					0 => Array (
+						'album_id' => '1',
+						'parent_id' => '0',
+						'left_id' => '0',
+						'right_id' => '0',
+						'album_parents' => '',
+						'album_type' => '1',
+						'album_status' => '0',
+						'album_contest' => '0',
+						'album_name' => 'TestPublicAlbum1',
+						'album_desc' => '',
+						'album_desc_options' => '7',
+						'album_desc_uid' => '',
+						'album_desc_bitfield' => '',
+						'album_user_id' => '0',
+						'album_images' => '3',
+						'album_images_real' => '3',
+						'album_last_image_id' => '3',
+						'album_image' => '',
+						'album_last_image_time' => '0',
+						'album_last_image_name' => 'TestImage3',
+						'album_last_username' => 'admin',
+						'album_last_user_colour' => '',
+						'album_last_user_id' => '2',
+						'album_watermark' => '1',
+						'album_sort_key' => '',
+						'album_sort_dir' => '',
+						'display_in_rrc' => '1',
+						'display_on_index' => '1',
+						'display_subalbum_list' => '1',
+						'album_feed' => '1',
+						'album_auth_access' => '0',
+					)
+				)
+			),
+			'album2'	=> array(
+				0, // branch user id
+				2, // album_id,
+				'all', // Type
+				'descending', // Order
+				true, // Include album
+				array( // expected
+					0	=> array(
+						'album_id' => '2',
+						'parent_id' => '1',
+						'left_id' => '1',
+						'right_id' => '3',
+						'album_parents' => 'a:1:{i:1;a:2:{i:0;s:8:"TestPublicAlbum1";i:1;i:1;}}',
+						'album_type' => '1',
+						'album_status' => '0',
+						'album_contest' => '0',
+						'album_name' => 'TestPublicAlbumSubAlbum1',
+						'album_desc' => '',
+						'album_desc_options' => '7',
+						'album_desc_uid' => '',
+						'album_desc_bitfield' => '',
+						'album_user_id' => '0',
+						'album_images' => '3',
+						'album_images_real' => '3',
+						'album_last_image_id' => '6',
+						'album_image' => '',
+						'album_last_image_time' => '0',
+						'album_last_image_name' => 'TestImage6',
+						'album_last_username' => 'admin',
+						'album_last_user_colour' => '',
+						'album_last_user_id' => '2',
+						'album_watermark' => '1',
+						'album_sort_key' => '',
+						'album_sort_dir' => '',
+						'display_in_rrc' => '0',
+						'display_on_index' => '1',
+						'display_subalbum_list' => '1',
+						'album_feed' => '1',
+						'album_auth_access' => '0',
+					),
+					1	=> array(
+						'album_id' => '3',
+						'parent_id' => '1',
+						'left_id' => '1',
+						'right_id' => '2',
+						'album_parents' => 'a:1:{i:1;a:2:{i:0;s:8:"TestPublicAlbum1";i:1;i:1;}}',
+						'album_type' => '1',
+						'album_status' => '0',
+						'album_contest' => '0',
+						'album_name' => 'TestPublicAlbumSubAlbum2',
+						'album_desc' => '',
+						'album_desc_options' => '7',
+						'album_desc_uid' => '',
+						'album_desc_bitfield' => '',
+						'album_user_id' => '0',
+						'album_images' => '3',
+						'album_images_real' => '3',
+						'album_last_image_id' => '9',
+						'album_image' => '',
+						'album_last_image_time' => '0',
+						'album_last_image_name' => 'TestImage9',
+						'album_last_username' => 'admin',
+						'album_last_user_colour' => '',
+						'album_last_user_id' => '2',
+						'album_watermark' => '1',
+						'album_sort_key' => '',
+						'album_sort_dir' => '',
+						'display_in_rrc' => '1',
+						'display_on_index' => '1',
+						'display_subalbum_list' => '1',
+						'album_feed' => '1',
+						'album_auth_access' => '0',
+					)
+				)
+			),
+			'album2_revert_no_album'	=> array(
+				0, // branch user id
+				2, // album_id,
+				'all', // Type
+				'asc', // Order
+				false, // Include album
+				array( // expected
+					0	=> array(
+						'album_id' => '3',
+						'parent_id' => '1',
+						'left_id' => '1',
+						'right_id' => '2',
+						'album_parents' => 'a:1:{i:1;a:2:{i:0;s:8:"TestPublicAlbum1";i:1;i:1;}}',
+						'album_type' => '1',
+						'album_status' => '0',
+						'album_contest' => '0',
+						'album_name' => 'TestPublicAlbumSubAlbum2',
+						'album_desc' => '',
+						'album_desc_options' => '7',
+						'album_desc_uid' => '',
+						'album_desc_bitfield' => '',
+						'album_user_id' => '0',
+						'album_images' => '3',
+						'album_images_real' => '3',
+						'album_last_image_id' => '9',
+						'album_image' => '',
+						'album_last_image_time' => '0',
+						'album_last_image_name' => 'TestImage9',
+						'album_last_username' => 'admin',
+						'album_last_user_colour' => '',
+						'album_last_user_id' => '2',
+						'album_watermark' => '1',
+						'album_sort_key' => '',
+						'album_sort_dir' => '',
+						'display_in_rrc' => '1',
+						'display_on_index' => '1',
+						'display_subalbum_list' => '1',
+						'album_feed' => '1',
+						'album_auth_access' => '0',
+					)
+				)
+			)
+		);
 	}
+	/**
+	 * test_get_branch
+	 *
+	 * @dataProvider get_branch_data
+	 */
+	public function test_get_branch($branch_user_id, $album_id, $type, $order, $include_album, $expected)
+	{
+		$this->assertEquals($expected, $this->display->get_branch($branch_user_id, $album_id, $type, $order, $include_album));
+	}
+	
+	
 }
