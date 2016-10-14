908fa9d38f55fbc7d8ba01133f55f2c2b0597f11
Pokyto <pokyto@users.noreply.github.com>
2016-10-14 17:08:38 +0200

    Update info_ucp_gallery.php

--- core/language/fr/info_ucp_gallery.php
+++ core/language/fr/info_ucp_gallery.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* @package Gallery -  info_ucp_gallery [French]
+* @package Gallery -  Info UCP Extension [French]
 * @copyright (c) 2012 nickvergessen - http://www.flying-bits.org/
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 * @translator fr (c) pokyto aka le.poke http://www.lestontonsfraggers.com inspired by darky - http://www.foruminfopc.fr/ and Team http://www.phpbb-fr.com/
