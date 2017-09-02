5155170d7c527df51afefae63fa233d49eba06d1
Stanislav Atanasov <lucifer@anavaro.com>
2017-09-02 23:57:15 +0300

    Travis is broken.

--- core/event/main_listener.php
+++ core/event/main_listener.php
@@ -87,15 +87,15 @@ class main_listener implements EventSubscriberInterface
 		if ($this->gallery_config->get('disp_total_images') == 1)
 		{
 			$this->template->assign_vars(array(
-				'PHPBBGALLERY_INDEX_STATS'	=> $this->gallery_config->get('num_images'),
+				'PHPBBGALLERY_INDEX_STATS' => $this->gallery_config->get('num_images'),
 			));
 		}
-		$lang_set_ext = $event['lang_set_ext'];
-		$lang_set_ext[] = array(
+		$data = $event['lang_set_ext'];
+		$data[] = array(
 			'ext_name'	=> 'phpbbgallery/core',
 			'lang_set'	=> array('info_acp_gallery', 'gallery', 'gallery_notifications', 'permissions_gallery'),
 		);
-		$event['lang_set_ext'] = $lang_set_ext;
+		$event['lang_set_ext'] = $data;
 	}
 	public function add_page_header_link($event)
 	{
