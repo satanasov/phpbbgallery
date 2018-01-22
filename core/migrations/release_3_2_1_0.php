e0bede1ddf6d718a08c8b16572e6788d550ddbe9
Stanislav Atanasov <s.atanasov@anavaro.com>
2018-01-22 02:39:10 +0200

    Fix routing error from 3.2.1.0 init release

--- core/migrations/release_3_2_1_0.php
+++ core/migrations/release_3_2_1_0.php
@@ -42,8 +42,8 @@ class release_3_2_1_0 extends \phpbb\db\migration\profilefield_base_migration
 	{
 		global $config;
 		$base_URI = generate_board_url();
-		$base_URI .= ($config['enable_mod_rewrite'] == 0 ? '/app.php/' : '');
-		$base_URI .= 'gallery/album/%s';
+		$base_URI .= ($config['enable_mod_rewrite'] == 0 ? '/app.php' : '');
+		$base_URI .= '/gallery/album/%s';
 		$sql = 'UPDATE ' . PROFILE_FIELDS_TABLE . ' SET field_contact_url = \'' . $base_URI . '\' WHERE field_name = \'gallery_palbum\'';
 		$this->db->sql_query($sql);
 	}
