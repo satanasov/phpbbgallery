68f5c69f0be2e161b412faeb328b4479b8fb2dbf
Stanislav Atanasov <s.atanasov@anavaro.com>
2020-02-20 17:00:03 +0200

    Return all tests and try adding slow ones

--- travis/run-tests.sh
+++ travis/run-tests.sh
@@ -11,10 +11,16 @@ set -x
 DB=$1
 TRAVIS_PHP_VERSION=$2
 EXTNAME=$3
+SLOW=$4
 
 if [ "$TRAVIS_PHP_VERSION" == "7.2" ] && [ "$DB" == "mysqli" ]
 then
-	phpBB/vendor/bin/phpunit --configuration phpBB/ext/"$EXTNAME"/travis/phpunit-"$DB"-travis.xml --bootstrap ./tests/bootstrap.php --coverage-clover build/logs/clover.xml
+	phpBB/vendor/bin/phpunit --configuration phpBB/ext/"$EXTNAME"/travis/phpunit-"$DB"-travis.xml --bootstrap ./tests/bootstrap.php --exclude-group functional,functional1 --coverage-clover build/logs/clover.xml
 else
-	phpBB/vendor/bin/phpunit --configuration phpBB/ext/"$EXTNAME"/travis/phpunit-"$DB"-travis.xml --bootstrap ./tests/bootstrap.php
+	phpBB/vendor/bin/phpunit --configuration phpBB/ext/"$EXTNAME"/travis/phpunit-"$DB"-travis.xml --bootstrap ./tests/bootstrap.php --exclude-group functional,functional1
+fi
+
+if [ "SLOW" == "1" ]
+then
+	phpBB/vendor/bin/phpunit --configuration phpBB/ext/"$EXTNAME"/travis/phpunit-"$DB"-travis.xml --bootstrap ./tests/bootstrap.php --group functional,functional1
 fi
