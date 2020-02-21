7e29fd71a561fde0c1727dda712fa8de64f80dc3
Stanislav Atanasov <s.atanasov@anavaro.com>
2020-02-21 10:51:39 +0200

    Try fixing Slow tests

--- travis/run-slow-tests.sh
+++ travis/run-slow-tests.sh
@@ -11,8 +11,9 @@ set -x
 DB=$1
 TRAVIS_PHP_VERSION=$2
 EXTNAME=$3
+SLOW=$4
 
-if [ "$TRAVIS_PHP_VERSION" == "7.1" ] && [ "$DB" == "mysqli" ]
+if [ "$TRAVIS_PHP_VERSION" == "7.1" ] && [ "$DB" == "mysqli" ] && [ "$SLOW" == "1" ]
 then
 	phpBB/vendor/bin/phpunit --configuration phpBB/ext/"$EXTNAME"/travis/phpunit-"$DB"-travis.xml --bootstrap ./tests/bootstrap.php --group functional,functional1
 fi
