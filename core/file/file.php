381f2b8a9a038db714bdeaa82ee467f63e7f92eb
Leinad4Mind <leinad4mind@gmail.com>
2025-07-28 15:30:03 +0100

    remove unnecessary htmlspecialchars

--- core/file/file.php
+++ core/file/file.php
@@ -251,8 +251,7 @@ class file
 	 */
 	public function header_filename($file)
 	{
-		$raw = $this->request->server('HTTP_USER_AGENT');
-		$user_agent = htmlspecialchars($raw);
+		$user_agent = $this->request->server('HTTP_USER_AGENT');
 
 		// There be dragons here.
 		// Not many follows the RFC...
